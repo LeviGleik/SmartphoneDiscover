dfcadd5e9f7621ab651f784cdc4117a0f7f1b777
levi gleik <leviacedo1@gmail.com>
2020-02-23 21:19:53 -0300

    Responsivity fixed

--- resources/views/smartphones/list.blade.php
+++ resources/views/smartphones/list.blade.php
@@ -2,48 +2,44 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-					Smartphones
-                	<div class="col-md-offset-2 float-right">
-                        <a class="btn btn-primary btn-sm" href="{{ url('smartphones/form') }}">Add</a>
-                    </div>
+        <div class="card">
+            <div class="card-header">
+				Smartphones
+            	<div class="col-md-offset-2 float-right">
+                    <a class="btn btn-primary btn-sm" href="{{ url('smartphones/form') }}">Add</a>
                 </div>
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <th>@sortablelink('brand', trans('brand'), ['filter' => 'active, visible'])</th>
-                            <th>@sortablelink('name')</th>
-                            <th>@sortablelink('year')</th>
-                            <th>@sortablelink('main_cam')</th>
-                            <th>@sortablelink('battery')</th>
-                            <th></th>
-                            <th></th>
-                        </thead>
-                        <tbody>
-                            @foreach($smartphones as $smartphone)
-                                <tr>
-                                    <td>{{ ucfirst($smartphone->brand) }}</td>
-                                    <td>{{ ucfirst($smartphone->name) }}</td>
-                                    <td>{{ $smartphone->year }}</td>
-                                    <td>{{ $smartphone->main_cam }}MP</td>
-                                    <td>{{ $smartphone->battery }}mAh</td>
-                                    <td>
-                                        <a class="btn btn-primary a-btn-slide-text btn-sm" href="/smartphones/{{$smartphone->id}}/edit"><i class="fas fa-edit fa-sm"></i></a>
-                                    </td>
-                                    <td>
-                                        {{ Form::open(['method' => 'delete', 'url' => 'smartphones/'.$smartphone->id]) }}
-                                            <button class="btn btn-danger a-btn-slide-text btn-sm" type="submit"><i class="fas fa-trash-alt fa-sm"></i></button>
-                                        {{ Form::close() }}
-                                    </td>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <th>@sortablelink('brand')</th>
+                        <th>@sortablelink('name')</th>
+                        <th>@sortablelink('main_cam', 'Main Camera')</th>
+                        <th>@sortablelink('year')</th>
+                        <th></th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        @foreach($smartphones as $smartphone)
+                            <tr>
+                                <td>{{ ucfirst($smartphone->brand) }}</td>
+                                <td>{{ ucfirst($smartphone->name) }}</td>
+                                <td>{{ $smartphone->main_cam }}MP</td>
+                                <td>{{ $smartphone->year }}</td>
+                                <td>
+                                    <a class="btn btn-primary a-btn-slide-text btn-sm" href="/smartphones/{{$smartphone->id}}/edit"><i class="fas fa-edit fa-sm"></i></a>
+                                </td>
+                                <td>
+                                    {{ Form::open(['method' => 'delete', 'url' => 'smartphones/'.$smartphone->id]) }}
+                                        <button class="btn btn-danger a-btn-slide-text btn-sm" type="submit"><i class="fas fa-trash-alt fa-sm"></i></button>
+                                    {{ Form::close() }}
+                                </td>
 
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{ $smartphones->appends(\Request::except('page'))->render() }}
-                </div>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $smartphones->appends(\Request::except('page'))->render() }}
             </div>
         </div>
     </div>
