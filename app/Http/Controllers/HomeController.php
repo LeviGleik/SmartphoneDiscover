8452bba7e5869e24024731d1e3b951c8bf686297
levigleik <leviacedo1@gmai.com>
2020-05-03 11:21:37 -0300

    advancedSearch started.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Smartphone;
+
 
 class HomeController extends Controller
 {
@@ -30,13 +33,30 @@ class HomeController extends Controller
     public function advanced(){
         return view('home.advanced');
     }
-    public function beginnerSave(){
+    public function beginnerSearch(){
         return view('home.beginner');
     }
-    public function intermediateSave(){
+    public function intermediateSearch(){
         return view('home.intermediate');
     }
-    public function advancedSave(){
-        return view('home.advanced');
+    public function advancedSearch(Request $request){
+        
+        $brand = $request->input('brand');
+        $name = $request->input('name');
+        $year = $request->input('year');
+        $chipset = $request->input('chipset');
+        $mem_ram = $request->input('mem_ram');
+        $mem_int = $request->input('mem_int');
+        $mem_exp_boolean = $request->input('mem_exp_boolean');
+        $display = $request->input('display');
+        $main_cam = $request->input('main_cam');
+        $selfie_cam = $request->input('selfie_cam');
+        $battery = $request->input('battery');
+        $price = $request->input('price');
+        $antutu = $request->input('antutu');
+
+        $data = Smartphone::where('brand', 'LIKE', "%{$brand}%")->get();
+
+        return view('smartphones.list');
     }
 }
